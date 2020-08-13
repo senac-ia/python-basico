723329552df451a8f21bb1eac70baeeaecfa8802
Celso Crivelaro <celsocrivelaro@gmail.com>
2020-08-13 16:07:05 -0300

    Refactoring

--- arvore.py
+++ arvore.py
@@ -1,7 +1,5 @@
 class No:
-    def __init__(self, direita, esquerda):
-        self.direta = direita
-        self.esquerda = esquerda
-
-class Arvore:
-    pass
+  def __init__(self, direita, esquerda, valor):
+    self.direta = direita
+    self.esquerda = esquerda
+    self.valor
--- pilha.py
+++ pilha.py
@@ -1,12 +1,12 @@
 class Pilha:
-    def __init__(self):
-        self.fila = []
-    
-    def push(self, item):
-        pass # implementar
-    
-    def pop(self):
-        pass # implementar
+  def __init__(self):
+    self.fila = []
 
-    def esta_vazio(self):
-        pass # implementar
+  def push(self, item):
+    pass # implementar
+
+  def pop(self):
+    pass # implementar
+
+  def esta_vazio(self):
+    pass # implementar
